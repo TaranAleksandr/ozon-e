c17ba46cc7f4d6697810ba6cc9188cd823258a54
TaranAleksandr <108215188+TaranAleksandr@users.noreply.github.com>
2022-12-01 01:46:34 +0300

    Модалка и webpack
    Подключил вывод модалки, настроил webpack, выполнил сборку. Работает

---
+++ src/modules/cart.js
@@ -0,0 +1,20 @@
+const cart = () => {
+
+  const cartBtn = document.getElementById('cart')
+  const modalCart = document.querySelector('.cart')
+  const modalClose = modalCart.querySelector('.cart-close')
+
+  console.dir(modalCart);
+
+  const openCart = () => {
+    modalCart.style.display = 'flex'
+  }
+  const closeCart = () => {
+    modalCart.style.display = ''
+  }
+
+  cartBtn.addEventListener('click', openCart)
+  modalClose.addEventListener('click', closeCart)
+}
+
+export default cart
